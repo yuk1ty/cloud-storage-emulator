b1c5e2e51ddb77ee51e4bc04f44aef62ed1c436f
yuk1ty <yuk1ty@users.noreply.github.com>
2024-06-22 23:26:38 +0900

    Add error handling aligned with real GCS

--- src/api/handlers/context/mod.rs
+++ src/api/handlers/context/mod.rs
@@ -1,14 +1,26 @@
-use axum::{extract::FromRef, http::StatusCode, response::IntoResponse};
+use axum::{extract::FromRef, http::StatusCode, response::IntoResponse, Json};
+use serde::Serialize;
 
 use crate::{libs::errors::Errors, storage::Storage};
+
+#[derive(Serialize)]
+#[serde(rename_all = "camelCase")]
+struct CloudStorageErrorResponse {
+    pub status_code: u16,
+    pub error_message: String,
+}
 
 impl IntoResponse for Errors {
     fn into_response(self) -> axum::response::Response {
         match self {
-            Errors::AlreadyExists { message } => {
-                tracing::error!(err.message = %message);
-                StatusCode::CONFLICT.into_response()
-            }
+            Errors::AlreadyExists { message } => (
+                StatusCode::CONFLICT,
+                Json(CloudStorageErrorResponse {
+                    status_code: StatusCode::CONFLICT.as_u16(),
+                    error_message: message,
+                }),
+            )
+                .into_response(),
             Errors::FailedToWriteStorage { id, message } => {
                 tracing::error!(err.message = %message, id);
                 StatusCode::INTERNAL_SERVER_ERROR.into_response()
@@ -23,7 +35,5 @@ impl IntoResponse for Errors {
 
 // This is needed to work with `axum_garde`.
 impl FromRef<Storage> for () {
-    fn from_ref(_: &Storage) -> Self {
-        
-    }
+    fn from_ref(_: &Storage) -> Self {}
 }
